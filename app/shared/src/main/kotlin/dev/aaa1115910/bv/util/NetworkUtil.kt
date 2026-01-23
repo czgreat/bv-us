decddd7935970ea471fa87c6605eb347b9bf85d4
czgreat <105542253+czgreat@users.noreply.github.com>
2026-01-23 14:58:09 +0800

    Update NetworkUtil.kt

--- app/shared/src/main/kotlin/dev/aaa1115910/bv/util/NetworkUtil.kt
+++ app/shared/src/main/kotlin/dev/aaa1115910/bv/util/NetworkUtil.kt
@@ -32,21 +32,7 @@ object NetworkUtil {
     }
 
     suspend fun isMainlandChina() = withContext(Dispatchers.IO) {
-        val deferreds = locCheckUrls.map { locCheckUrl ->
-            async {
-                runCatching {
-                    val result = client.get(locCheckUrl).bodyAsText()
-                    logger.info { "Network result:\n$result" }
-
-                    val networkCheckResult = result
-                        .lines()
-                        .filter { it.isNotBlank() }
-                        .associate { with(it.split("=")) { this[0] to this[1] } }
-
-                    require(networkCheckResult["loc"] != "CN") { "BV doesn't support use in mainland China" }
-                    false
-                }.getOrDefault(true)
-            }
+        false
         }
 
         select {
